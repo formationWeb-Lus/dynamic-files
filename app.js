082c18cfdf62e664a34fc05bc1ca99d9fe7b17be
formationWeb-Lus <jireselusa127@gmail.com>
2026-01-01 01:26:13 -0800

    add

--- app.js
+++ app.js
@@ -18,11 +18,27 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-// **Simuler une table SQL pour images** (tu peux remplacer par vraie requête SQL)
+// **Table simulée pour images** (tu peux remplacer par vraie requête SQL)
 const images = [
-    { name: 'Logo', url: 'https://raw.githubusercontent.com/username/repo/master/images/logo.png' },
-    { name: 'Cover', url: 'https://raw.githubusercontent.com/username/repo/master/images/cover.png' },
-    { name: 'Aerocar', url: 'https://raw.githubusercontent.com/formationWeb-Lus/web_backend/main/public/images/vehicles/aerocar.jpg' }
+    { name: 'AppelCréationdeFonctions', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/AppelCr%C3%A9ationdeFonctions.jpg?raw=true' },
+    { name: 'AuthentificationetAutorisation', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/AuthentificationetAutorisation.jpg?raw=true' },
+    { name: 'CSharp', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/CSharp.png?raw=true' },
+    { name: 'InsertionetValidationdesDonnées', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/InsertionetValidationdesDonn%C3%A9es.png?raw=true' },
+    { name: 'IntroductionàPython', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/Introduction%20%C3%A0%20Python.jpg?raw=true' },
+    { name: 'IntroductionàlaProgrammation', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/Introduction%C3%A0laProgrammation.jpg?raw=true' },
+    { name: 'JavaScriptAvancé', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/JavaScriptAvanc%C3%A9.jpg?raw=true' },
+    { name: 'MVCetLivraisondeContenu', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/MVCetLivraisondeContenu.jpg?raw=true' },
+    { name: 'OAuthetSécuritédesAPI', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/OAuthetS%C3%A9curit%C3%A9desAPI.jpg?raw=true' },
+    { name: 'ProgrammationaveclesFonctionsIntroduction', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/Pr4ogrammationaveclesFonctionsIntroduction.png?raw=true' },
+    { name: 'ProgrammationAvancéeCSharp', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/ProgrammationAvanc%C3%A9eC%23.png?raw=true' },
+    { name: 'RESTAlternativesetValidation', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/RESTAlternativesetValidation.jpg?raw=true' },
+    { name: 'ReactFondamentauxetProjetsInitiaux', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/ReactFondamentauxetProjetsInitiaux.jpg?raw=true' },
+    { name: 'RequêtesHTTPetDocumentationdAPI', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/Requ%C3%AAtesHTTPetDocumentationdAPI.jpg?raw=true' },
+    { name: 'TestsDébogageetGestiondesErreurs', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/TestsD%C3%A9bogageetGestiondesErreurs.jpg?raw=true' },
+    { name: 'WDD130WebFundamentals', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/WDD130WebFundamentals.jpg?raw=true' },
+    { name: 'Web-Backend', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/Web-Backend.jpg?raw=true' },
+    { name: 'WebFrontend', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/WebFrontend.jpg?raw=true' },
+    { name: 'WebFull-StackDéveloppementIntroduction', url: 'https://github.com/formationWeb-Lus/coderise-images/blob/main/images/WebFull-StackD%C3%A9veloppementIntroduction.png?raw=true' }
 ];
 
 // Route principale
